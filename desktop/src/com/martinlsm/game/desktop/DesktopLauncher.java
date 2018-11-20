081005ebf5a64f687f87a249123b05c221494579
martinlsm <martin.lindstrom94@gmail.com>
2018-11-20 21:40:14 +0100

    Setup project

---
+++ desktop/src/com/martinlsm/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.martinlsm.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.martinlsm.game.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
